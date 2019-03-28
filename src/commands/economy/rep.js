3f3b7276ed0f5cde76cdc6f1cd9ab46affa6d062
sundaind <49017163+sundaind@users.noreply.github.com>
2019-03-28 10:33:01 +0000

    Update rep.js

--- src/commands/economy/rep.js
+++ src/commands/economy/rep.js
@@ -14,19 +14,19 @@ exports.run = async(client, message, args, color) => {
         let timeObj = ms(cooldown - (Date.now() - lastrep))
         let eh = require('../../../src/handle/cooldownAns.json');
         let ops = eh[Math.floor(Math.random() * eh.length)];
-        message.channel.send(`**${message.author.username}**, ${ops} (Ratelimited)\n**You'll be able give reputation point again in ${timeObj.hours} hours, ${timeObj.minutes} minutes and ${timeObj.seconds} seconds**`)
+        message.channel.send(`**${message.author.username}**, ${ops} (Ratelimited)\n**Kamu dapat memberikan Rep lagi dalam ${timeObj.hours} hours, ${timeObj.minutes} minutes and ${timeObj.seconds} seconds**`)
     
   } else {
     
-    if(!args[0]) return message.channel.send(`**${message.author.username}**, You can rep someone now,\nYou need to mention user to be given reputation!`);
+    if(!args[0]) return message.channel.send(`**${message.author.username}**, Kamu bisa menggunakan Rep Sekarang,Ayo Rep`);
     try {
     let user = message.mentions.users.first() || client.users.get(args[0]);
-    if (user.bot) return message.channel.send(`**${message.author.username}**, You cannot rep a bot`);
-    if (user.id == message.author.id) return message.channel.send(`**${message.author.username}**, You cannot rep yourselft!`);
+    if (user.bot) return message.channel.send(`**${message.author.username}**, Kamu gak bisa memberikan Rep kepada BOT`);
+    if (user.id == message.author.id) return message.channel.send(`**${message.author.username}**, Kamu tidak dapat Rep Sendiri!`);
       
       db.set(`lastRep_${message.author.id}`, Date.now());        
       db.add(`rep_${user.id}`, 1)
-        message.channel.send(`**🎖️ | Hey <@${user.id}>, You got reputation points from ${message.author.tag}**`);
+        message.channel.send(`**🎖️ | Hallo kak <@${user.id}>, Kamu mendapat Reputasi Poin dari ${message.author.tag}**`);
     } catch (e) {
       message.channel.send(e.message);
     } 
@@ -40,7 +40,7 @@ exports.conf = {
 
 exports.help = {
     name: 'rep',
-    description: 'Give someone reputation point',
-    usage: 'rep <@SomeOne | id>', 
-    example: ['rep @Yatogami', 'rep 123456789']
+    description: 'Berikan Seseorang Reputasi Poin',
+    usage: 'rep <@pengguna | id>', 
+    example: ['rep @Ansel', 'rep 123456789']
 }
